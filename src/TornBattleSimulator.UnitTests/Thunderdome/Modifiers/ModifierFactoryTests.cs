70f2aeebaa548e1cccf6c9c58bad9b8eb4a3d694
Marches <recettear.ho@gmail.com>
2024-07-26 19:54:44 +0100

    ModifierFactoryTests

---
+++ src/TornBattleSimulator.UnitTests/Thunderdome/Modifiers/ModifierFactoryTests.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using TornBattleSimulator.Battle.Thunderdome.Modifiers;
+using TornBattleSimulator.Core.Build.Equipment;
+
+namespace TornBattleSimulator.UnitTests.Thunderdome.Modifiers;
+
+[TestFixture]
+public class ModifierFactoryTests
+{
+    [Test]
+    public void GetModifier_ReturnsCorrectModifier([Values] ModifierType modifierType)
+    {
+        new ModifierFactory().GetModifier(modifierType, 50).Effect.Should().Be(modifierType);
+    }
+}
